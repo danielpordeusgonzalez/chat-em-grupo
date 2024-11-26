81b14e60c1fe93c97ff6a8711b06d30c35564765
danielpordeusg <danielpordeusgonzalez@hotmail.com>
2024-11-26 10:54:21 -0300

    feat: cria o server básico que recebe as mensagem do client

--- server.py
+++ server.py
@@ -1,1 +1,16 @@
 import socket
+
+HOST = "localhost"
+PORT = 8002
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+sock.bind((HOST, PORT))
+
+sock.listen(8)
+
+while True:
+    novoSock, _ = sock.accept()
+    mensagem = novoSock.recv(1024).decode()
+    print(mensagem)
+    novoSock.send(b"ok")
